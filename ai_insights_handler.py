8facc13ee2b84e6834d44181c3f7297bbb29bcd3
Mohit Vaidya <142785279+vaidyamohit@users.noreply.github.com>
2025-03-25 17:36:41 +0530

    ai_insights_handler.py

--- ai_insights_handler.py
+++ ai_insights_handler.py
@@ -7,7 +7,7 @@ class AIInsights:
     def __init__(self, api_key, model_choice):
         self.api_key = api_key
         self.model_choice = model_choice
-
+'''
         if model_choice == "Gemini":
             genai.configure(api_key=self.api_key)
             self.model = genai.GenerativeModel(model_name="gemini-2.0-flash")
@@ -15,6 +15,14 @@ class AIInsights:
             self.model = anthropic.Anthropic(api_key=self.api_key)
         elif model_choice == "OpenAI":
             openai.api_key = self.api_key
+'''
+        if model_choice == "Gemini":
+            ai_insights_obj = AIInsights(AIzaSyAVi1v80vt41mTjZED6BaMs5-74HKFkSk0, model_choice)
+        elif model_choice == "OpenAI":
+            ai_insights_obj = AIInsights(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, model_choice)
+        elif model_choice == "Claude":
+            ai_insights_obj = AIInsights(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, model_choice)
+
 
     def get_ai_insights(self, image_path, stock, market):
         image = PIL.Image.open(image_path)
